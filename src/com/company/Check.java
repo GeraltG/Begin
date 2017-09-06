9f23ff2762b760584086002802503120878837d5
GCL <gucl713@gmail.com>
2017-09-06 18:49:54 +0800

    简单的登录

---
+++ src/com/company/Check.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by gcl713 on 2017/9/6.
+ */
+public class Check {
+    public boolean vaildate(String name,String password)
+    {
+        if (name.equals("aaa")&&password.equals("123456"))
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/com/company/DEMO3.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by gcl713 on 2017/9/6.
+ */
+public class DEMO3 {
+    public static void main(String args[])
+    {
+        Operate oper=new Operate(args);
+        System.out.println(oper.login());
+    }
+}
